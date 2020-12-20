51d617ca3f400c85132f37539d94672a1b55a327
webprogrammingunpas <fathurrahmanf123@gmail.com>
2020-12-21 00:10:48 +0700

    create header component and put to arrange LandingPage

--- src/elements/pages/LandingPage.js
+++ src/elements/pages/LandingPage.js
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function LandingPage() {
-  return <div></div>;
+import Header from "elements/parts/Header";
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <>
+        {/* ...this.props = sintak es6 sprate operator / ketika ada props di class ini dia akan di parsing ke component header */}
+        <Header {...this.props}></Header>
+      </>
+    );
+  }
 }
