2b806d6c85d958e076c1d671c0406a4ba09fdbfc
webprogrammingunpas <fathurrahmanf123@gmail.com>
2021-01-25 01:00:35 +0700

    finished component part mostpicked & categories

--- src/elements/pages/LandingPage.js
+++ src/elements/pages/LandingPage.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 
 import Header from "elements/parts/Header";
+import landingPage from "elements/json/landingPage.json";
+import Hero from "elements/parts/Hero";
+
+import MostPicked from "elements/parts/MostPicked";
+import Categories from "elements/parts/Categories";
 
 export default class LandingPage extends Component {
+  //constructor akan berjalan paling pertama ketika kita meruning class
+  constructor(props){
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
+
+
   render() {
+    // console.log(this.props)
     return (
       <>
         {/* ...this.props = sintak es6 sprate operator / ketika ada props di class ini dia akan di parsing ke component header */}
         <Header {...this.props}></Header>
+
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked  refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
+        <Categories data={landingPage.categories}/>
       </>
     );
   }
