de1690a4362d101e876008b70913d610f0c868cf
fathurzoy <fathurahmanf123@gmail.com>
2021-04-09 18:04:47 +0700

    Integrasi checkout page finish

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -4,13 +4,20 @@ import { connect } from "react-redux";
 
 import Header from "parts/Header";
 import Button from "elements/Button";
-import Stepper, { Numbering, Meta, MainContent, Controller } from "elements/Stepper";
+import Stepper, {
+  Numbering,
+  Meta,
+  MainContent,
+  Controller,
+} from "elements/Stepper";
 
 import BookingInformation from "parts/Checkout/BookingInformation";
 import Payment from "parts/Checkout/Payment";
 import Completed from "parts/Checkout/Completed";
 
 import ItemDetails from "json/itemDetails.json";
+
+import { submitBooking } from "store/actions/checkout";
 
 class Checkout extends Component {
   state = {
@@ -36,22 +43,57 @@ class Checkout extends Component {
 
   componentDidMount() {
     window.scroll(0, 0);
-    document.title = "Staycation | Checkout";
   }
+
+  _Submit = (nextStep) => {
+    const { data } = this.state;
+    const { checkout } = this.props;
+
+    const payload = new FormData();
+    payload.append("firstName", data.firstName);
+    payload.append("lastName", data.lastName);
+    payload.append("email", data.email);
+    payload.append("phoneNumber", data.phone);
+    payload.append("idItem", checkout._id);
+    payload.append("duration", checkout.duration);
+    payload.append("bookingStartDate", checkout.date.startDate);
+    payload.append("bookingEndDate", checkout.date.endDate);
+    payload.append("accountHolder", data.bankHolder);
+    payload.append("bankFrom", data.bankName);
+    payload.append("image", data.proofPayment[0]);
+    // payload.append("bankId", checkout.bankId);
+
+    this.props
+      .submitBooking(payload)
+      .then(() => {
+        nextStep();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   render() {
     const { data } = this.state;
     const { checkout, page } = this.props;
-    console.log(page);
+    console.log(page, data);
 
     if (!checkout) {
       return (
         <div className="container">
-          <div className="row align-items-center justify-content-center text-center" style={{ height: "100vh" }}>
+          <div
+            className="row align-items-center justify-content-center text-center"
+            style={{ height: "100vh" }}
+          >
             <div className="col-3">
               Pilih kamar dulu
               <div>
-                <Button className="btn mt-5" type="button" onClick={() => this.props.history.goBack()} isLight>
+                <Button
+                  className="btn mt-5"
+                  type="button"
+                  onClick={() => this.props.history.goBack()}
+                  isLight
+                >
                   Back
                 </Button>
               </div>
@@ -68,12 +110,26 @@ class Checkout extends Component {
       bookingInformation: {
         title: "Booking Information",
         description: "Please fill up the blank fields below",
-        content: <BookingInformation data={data} checkout={checkout} ItemDetails={page[checkout._id]} onChange={this.onChange} />,
+        content: (
+          <BookingInformation
+            data={data}
+            checkout={checkout}
+            ItemDetails={page[checkout._id]}
+            onChange={this.onChange}
+          />
+        ),
       },
       payment: {
         title: "Payment",
         description: "Kindly follow the instructions below",
-        content: <Payment data={data} ItemDetails={page[checkout._id]} checkout={checkout} onChange={this.onChange} />,
+        content: (
+          <Payment
+            data={data}
+            ItemDetails={page[checkout._id]}
+            checkout={checkout}
+            onChange={this.onChange}
+          />
+        ),
       },
       completed: {
         title: "Yay! Completed",
@@ -89,7 +145,11 @@ class Checkout extends Component {
         <Stepper steps={steps}>
           {(prevStep, nextStep, CurrentStep, steps) => (
             <>
-              <Numbering data={steps} current={CurrentStep} style={{ marginBottom: 50 }} />
+              <Numbering
+                data={steps}
+                current={CurrentStep}
+                style={{ marginBottom: 50 }}
+              />
 
               <Meta data={steps} current={CurrentStep} />
 
@@ -97,14 +157,30 @@ class Checkout extends Component {
 
               {CurrentStep === "bookingInformation" && (
                 <Controller>
-                  {data.firstName !== "" && data.lastName !== "" && data.email !== "" && data.phone !== "" && (
-                    <Fade>
-                      <Button className="btn mb-3" type="button" isBlock isPrimary hasShadow onClick={nextStep}>
-                        Continue to Book
-                      </Button>
-                    </Fade>
-                  )}
-                  <Button className="btn" type="link" isBlock isLight href={`/properties/${checkout._id}`}>
+                  {data.firstName !== "" &&
+                    data.lastName !== "" &&
+                    data.email !== "" &&
+                    data.phone !== "" && (
+                      <Fade>
+                        <Button
+                          className="btn mb-3"
+                          type="button"
+                          isBlock
+                          isPrimary
+                          hasShadow
+                          onClick={nextStep}
+                        >
+                          Continue to Book
+                        </Button>
+                      </Fade>
+                    )}
+                  <Button
+                    className="btn"
+                    type="link"
+                    isBlock
+                    isLight
+                    href={`/properties/${checkout._id}`}
+                  >
                     Cancel
                   </Button>
                 </Controller>
@@ -112,14 +188,29 @@ class Checkout extends Component {
 
               {CurrentStep === "payment" && (
                 <Controller>
-                  {data.proofPayment !== "" && data.bankName !== "" && data.bankHolder !== "" && (
-                    <Fade>
-                      <Button className="btn mb-3" type="button" isBlock isPrimary hasShadow onClick={nextStep}>
-                        Continue to Book
-                      </Button>
-                    </Fade>
-                  )}
-                  <Button className="btn" type="button" isBlock isLight onClick={prevStep}>
+                  {data.proofPayment !== "" &&
+                    data.bankName !== "" &&
+                    data.bankHolder !== "" && (
+                      <Fade>
+                        <Button
+                          className="btn mb-3"
+                          type="button"
+                          isBlock
+                          isPrimary
+                          hasShadow
+                          onClick={() => this._Submit(nextStep)}
+                        >
+                          Continue to Book
+                        </Button>
+                      </Fade>
+                    )}
+                  <Button
+                    className="btn"
+                    type="button"
+                    isBlock
+                    isLight
+                    onClick={prevStep}
+                  >
                     Cancel
                   </Button>
                 </Controller>
@@ -127,7 +218,14 @@ class Checkout extends Component {
 
               {CurrentStep === "completed" && (
                 <Controller>
-                  <Button className="btn" type="link" isBlock isPrimary hasShadow href="">
+                  <Button
+                    className="btn"
+                    type="link"
+                    isBlock
+                    isPrimary
+                    hasShadow
+                    href=""
+                  >
                     Back to Home
                   </Button>
                 </Controller>
@@ -145,4 +243,4 @@ const mapStateToProps = (state) => ({
   page: state.page,
 });
 
-export default connect(mapStateToProps)(Checkout);
+export default connect(mapStateToProps, { submitBooking })(Checkout);
